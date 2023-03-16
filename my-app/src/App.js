e80f7508bd4f026d1320116b0c1a78cfef14df1c
LYT9962 <321835650@qq.com>
2023-03-16 21:51:53 +0800

    了解jsx语法

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,24 @@
 function App() {
+
+    const red = {
+        color:"red"
+    }
+
+    const elem = (<div>李凯旋</div>);
+
+    const list = [(<div key={1}>大话西游</div>), (<span key={2}>从你的全世界路过</span>),(<i key={3}>乘风破浪</i>)]
   return (
-      <div>Hello, React</div>
+      <div>
+        <div>Hello, React</div>
+        <ul>
+            <li id="sang">张三</li>
+            <li id="zhang">李四</li>
+            <li  className={'haha'}>王五</li>
+            <li style={red}>王二麻子</li>
+        </ul>
+          {elem}
+          {list}
+      </div>
   );
 }
 
